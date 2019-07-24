c7f79a47aad0b32e4a8d12098c67ed8ec6af20a8
Jason Crowley <crowley.json@gmail.com>
2019-07-24 14:52:43 -0400

    Add submit button and make form flex

--- src/components/Surveys/Surveys.js
+++ src/components/Surveys/Surveys.js
@@ -1,36 +1,48 @@
 import React, { useState } from 'react';
+import './Surveys.css';
 
 export default function Surveys(props) {
+  // Handle search state
   const [search, setSearch] = useState('');
   const handleChangeSearch = e => {
     setSearch(e.target.value);
   };
 
+  // Handle sort state
   const [sort, setSort] = useState('');
   const handleChangeSort = e => {
     setSort(e.target.value);
   };
 
+  // Handle filter state
   const [filter, setFilter] = useState('');
   const handleChangeFilter = e => {
     setFilter(e.target.value);
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert(`Search: ${search}, Sort: ${sort}, Filter: ${filter}`);
+  };
+
   return (
     <div>
       <h1>Surveys</h1>
-      <form>
-        <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form__input">
           <label for="search">Search:{' '}</label>
           <input type="text" name="search" value={search} onChange={handleChangeSearch} />
         </div>
-        <div>
+        <div className="form__input">
           <label for="sort">Sort By:{' '}</label>
           <input type="text" name="sort" value={sort} onChange={handleChangeSort} />
         </div>
-        <div>
+        <div className="form__input">
           <label for="filter">Filter:{' '}</label>
           <input type="text" name="filter" value={filter} onChange={handleChangeFilter} />
+        </div>
+        <div className="form__input">
+          <input type="submit" />
         </div>
       </form>
     </div>
