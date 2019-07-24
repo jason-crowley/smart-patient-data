a5cf7d23a61d19016ff589cdf14ee2fde1a3956b
Jason Crowley <crowley.json@gmail.com>
2019-07-24 14:30:53 -0400

    Make simple form for searching surveys

--- src/components/Surveys/Surveys.js
+++ src/components/Surveys/Surveys.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Surveys extends Component {
-  render() {
-    return <div>Surveys</div>
-  }
+export default function Surveys(props) {
+  const [search, setSearch] = useState('');
+  const handleChangeSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const [sort, setSort] = useState('');
+  const handleChangeSort = e => {
+    setSort(e.target.value);
+  };
+
+  const [filter, setFilter] = useState('');
+  const handleChangeFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Surveys</h1>
+      <form>
+        <div>
+          <label for="search">Search:{' '}</label>
+          <input type="text" name="search" value={search} onChange={handleChangeSearch} />
+        </div>
+        <div>
+          <label for="sort">Sort By:{' '}</label>
+          <input type="text" name="sort" value={sort} onChange={handleChangeSort} />
+        </div>
+        <div>
+          <label for="filter">Filter:{' '}</label>
+          <input type="text" name="filter" value={filter} onChange={handleChangeFilter} />
+        </div>
+      </form>
+    </div>
+  );
 }
-
-export default Surveys;
