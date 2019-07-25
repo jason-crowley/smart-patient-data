7113ba35868282f07156f3bd0743a44d1b647adf
Jason Crowley <crowley.json@gmail.com>
2019-07-25 17:18:39 -0400

    Add simple survey builder layout

--- src/components/Builder/index.js
+++ src/components/Builder/index.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Builder.css';
 
-class Builder extends Component {
-  render() {
-    return <div>Builder</div>
-  }
-}
+export default function Builder(props) {
 
-export default Builder;
+  return (
+    <div className="Builder">
+      <h1>Builder</h1>
+      <div className="Group">
+        <div>
+          <label>Group {' '}
+            <input type="number" defaultValue="1" />: {' '}
+          </label>
+          <input type="text" placeholder="Enter group name here" />
+        </div>
+        <textarea placeholder="Enter text for group description here" />
+        <div className="Question">
+          <div>
+            <label>Question {' '}
+              <input type="number" defaultValue="1" />: {' '}
+            </label>
+            <input type="text" placeholder="Enter group name here" />
+          </div>
+          <textarea placeholder="Enter text for question here" />
+        </div>
+        <button>+ Question</button>
+      </div>
+      <div className="Group">
+        <div>
+          <label>Group {' '}
+            <input type="number" defaultValue="2" />: {' '}
+          </label>
+          <input
+            type="text"
+            defaultValue="Treatment Feedback"
+            placeholder="Enter group name here"
+          />
+        </div>
+        <textarea placeholder="Enter text for group description here" />
+        <button>+ Question</button>
+      </div>
+    </div>
+  );
+};
