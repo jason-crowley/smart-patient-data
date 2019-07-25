b82d1fb19c4be89746d60b5b0aee0761fc15f772
Jason Crowley <crowley.json@gmail.com>
2019-07-25 09:47:09 -0400

    Fix JSX 'for' attribute warning

--- src/components/Surveys/SurveySearchForm/index.js
+++ src/components/Surveys/SurveySearchForm/index.js
@@ -12,15 +12,21 @@ export default function SurveySearchForm(props) {
   return (
     <form className="form" onSubmit={handleSubmit}>
       <div className="form__input">
-        <label for="search">Search:{' '}</label>
-        <input type="text" name="search" value={search} onChange={handleChange} />
+        <label htmlFor="search">Search:{' '}</label>
+        <input
+          type="text"
+          name="search"
+          value={search}
+          placeholder="Search for existing surveys"
+          onChange={handleChange}
+        />
       </div>
       <div className="form__input">
-        <label for="sort">Sort By:{' '}</label>
+        <label htmlFor="sort">Sort By:{' '}</label>
         <input type="text" name="sort" value={sort} onChange={handleChange} />
       </div>
       <div className="form__input">
-        <label for="filter">Filter:{' '}</label>
+        <label htmlFor="filter">Filter:{' '}</label>
         <input type="text" name="filter" value={filter} onChange={handleChange} />
       </div>
       <div className="form__input">
