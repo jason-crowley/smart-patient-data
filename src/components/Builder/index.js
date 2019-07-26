c4efa679234bed9c289ce634c2e5a20a633fd197
Jason Crowley <crowley.json@gmail.com>
2019-07-25 20:36:34 -0400

    Make form element surrounding survey builder

--- src/components/Builder/index.js
+++ src/components/Builder/index.js
@@ -2,43 +2,44 @@ import React from 'react';
 import './Builder.css';
 
 export default function Builder(props) {
-
   return (
     <div className="Builder">
       <h1>Builder</h1>
-      <div className="Group">
-        <div>
-          <label>Group {' '}
-            <input type="number" defaultValue="1" />: {' '}
-          </label>
-          <input type="text" placeholder="Enter group name here" />
-        </div>
-        <textarea placeholder="Enter text for group description here" />
-        <div className="Question">
+      <form className="BuilderForm">
+        <div className="Group">
           <div>
-            <label>Question {' '}
+            <label>Group {' '}
               <input type="number" defaultValue="1" />: {' '}
             </label>
             <input type="text" placeholder="Enter group name here" />
           </div>
-          <textarea placeholder="Enter text for question here" />
+          <textarea placeholder="Enter text for group description here" />
+          <div className="Question">
+            <div>
+              <label>Question {' '}
+                <input type="number" defaultValue="1" />: {' '}
+              </label>
+              <input type="text" placeholder="Enter question name here" />
+            </div>
+            <textarea placeholder="Enter text for question here" />
+          </div>
+          <button>+ Question</button>
         </div>
-        <button>+ Question</button>
-      </div>
-      <div className="Group">
-        <div>
-          <label>Group {' '}
-            <input type="number" defaultValue="2" />: {' '}
-          </label>
-          <input
-            type="text"
-            defaultValue="Treatment Feedback"
-            placeholder="Enter group name here"
-          />
+        <div className="Group">
+          <div>
+            <label>Group {' '}
+              <input type="number" defaultValue="2" />: {' '}
+            </label>
+            <input
+              type="text"
+              defaultValue="Treatment Feedback"
+              placeholder="Enter group name here"
+            />
+          </div>
+          <textarea placeholder="Enter text for group description here" />
+          <button>+ Question</button>
         </div>
-        <textarea placeholder="Enter text for group description here" />
-        <button>+ Question</button>
-      </div>
+      </form>
     </div>
   );
 };
