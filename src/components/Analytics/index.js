f61392f3ce3b0e5cc5f3264b3b571a27ae08845a
Jason Crowley <crowley.json@gmail.com>
2019-07-30 09:05:33 -0400

    Add template sections to analytics page

--- src/components/Analytics/index.js
+++ src/components/Analytics/index.js
@@ -5,6 +5,36 @@ export default function Analytics(props) {
   return (
     <div className="Analytics">
       <h1>Analytics</h1>
+      <div className="Analytics__pghd">
+        <h2>PGHD</h2>
+        <div>
+          <h3>Surveys Chart</h3>
+        </div>
+        <div>
+          <h3>Activity Chart</h3>
+        </div>
+        <div>
+          <h3>Sleep Chart</h3>
+        </div>
+        <div>
+          <h3>Blood Pressure Chart</h3>
+        </div>
+      </div>
+      <div className="Analytics__ehr">
+        <h2>EHR</h2>
+        <div>
+          <h3>Blood Pressure</h3>
+        </div>
+        <div>
+          <h3>Conditions</h3>
+        </div>
+        <div>
+          <h3>Lab Test (Glucose)</h3>
+        </div>
+        <div>
+          <h3>Body Weight</h3>
+        </div>
+      </div>
     </div>
   );
 };
