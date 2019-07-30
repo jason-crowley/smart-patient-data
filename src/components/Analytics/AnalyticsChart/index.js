b4f8827fae8201fb6bce5c50f3110b1b61df5ab8
Jason Crowley <crowley.json@gmail.com>
2019-07-30 16:48:07 -0400

    Arrange charts in two-column grid and center their titles

--- src/components/Analytics/AnalyticsChart/index.js
+++ src/components/Analytics/AnalyticsChart/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
 import moment from 'moment';
+import './AnalyticsChart.css';
 
 export default function AnalyticsChart({ title, data }) {
   return (
     <div>
-      <h3>{title}</h3>
+      <h3 className="AnalyticsChart__header">{title}</h3>
       <LineChart width={300} height={200} data={data}>
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis
           dataKey="date"
           tickFormatter={date => moment(date).format('MM/DD')}
-          padding={{ left: 10, right: 10 }}
+          padding={{ left: 20, right: 20 }}
         />
         <YAxis />
         <Tooltip />
