6df935b65b4910d024015bacddc37aa67156873f
Jason Crowley <crowley.json@gmail.com>
2019-08-01 15:36:02 -0400

    Add handling QuestionnaireResponses in ResponseItem factory method

--- src/models/ResponseItem/index.js
+++ src/models/ResponseItem/index.js
@@ -15,14 +15,29 @@ export default class ResponseItem {
 
   static from(resource) {
     switch (resource.resourceType) {
-      case 'Observation':
+      case 'Observation': {
         const {
           id,
           code,
           effectiveDateTime: date,
-          valueQuantity: { value }
+          valueQuantity: { value } = {}
         } = resource;
         return new ResponseItem({ id, date, code, value });
+      }
+      case 'QuestionnaireResponse': {
+        const hits = [];
+        const stack = [resource];
+        const { authored: date } = resource;
+        while (stack.length) {
+          const { linkId: id, answer, item } = stack.pop();
+          if (item) stack.push(...item);
+          if (!answer) continue;
+          Object.values(answer).forEach(value => {
+            hits.push(new ResponseItem({ id, date, value, code: 'unknown' }));
+          });
+        }
+        return hits;
+      }
       default:
         throw new Error(
           `Type '${resource.resourceType}' is not supported at this time.`
