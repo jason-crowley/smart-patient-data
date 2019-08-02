461d866d887368081a3b768ba2c09443555db48d
Jason Crowley <crowley.json@gmail.com>
2019-08-02 13:58:01 -0400

    Add static factory method for Event

--- src/models/Event/index.js
+++ src/models/Event/index.js
@@ -1,15 +1,48 @@
-// const required = () => { throw new Error('Missing required parameter'); };
+const required = () => { throw new Error('Missing required parameter'); };
 
 export default class Event {
   constructor({
-    startDate,
-    endDate,
+    category = required(),
     type,
-    description,
+    code = required(),
+    startDate = required(),
+    endDate = startDate,
   } = {}) {
+    this.category = category;
+    this.type = type;
+    this.code = code;
     this.startDate = startDate;
     this.endDate = endDate;
-    this.type = type;
-    this.description = description;
+  }
+
+  static from(resource) {
+    const { resourceType: category } = resource;
+    switch (category) {
+      case 'MedicationRequest': {
+        const {
+          // category: [{}] = [{}],
+          medicationCodeableConcept: code,
+          authoredOn: startDate,
+        } = resource;
+        return new Event({ category, code, startDate });
+      }
+      case 'Condition': {
+        const {
+          code,
+          onsetDateTime: startDate,
+          abatementDateTime: endDate,
+        } = resource;
+        return new Event({ category, code, startDate, endDate });
+      }
+      case 'Encounter': {
+        const {
+          type: code,
+          period: [{ start: startDate, end: endDate }] = [{}],
+        } = resource;
+        return new Event({ category, code, startDate, endDate });
+      }
+      default:
+        throw new Error(`Category '${category}' is not supported at this time.`);
+    }
   }
 };
