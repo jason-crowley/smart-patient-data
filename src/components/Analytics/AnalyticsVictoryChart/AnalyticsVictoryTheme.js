32892f73c6cae9eaabe3c22f715fdd04b97ec9b7
Jason Crowley <crowley.json@gmail.com>
2019-08-09 11:02:32 -0400

    Move AnalyticsVictoryTheme to separate file

---
+++ src/components/Analytics/AnalyticsVictoryChart/AnalyticsVictoryTheme.js
@@ -0,0 +1,38 @@
+export default {
+  axis: {
+    style: {
+      axis: { stroke: '#666' },
+      grid: { stroke: '#ccc', strokeDasharray: '5 5' },
+      ticks: { size: 6, stroke: '#666' },
+      tickLabels: { fill: '#666' },
+    },
+  },
+  // bar: {},
+  chart: {
+    domainPadding: 30,
+    padding: { top: 50, right: 50, bottom: 60, left: 60 },
+  },
+  legend: {
+    orientation: 'horizontal',
+    style: {
+      border: { stroke: 'black' },
+      labels: { fontSize: 12 },
+    },
+  },
+  line: {
+    interpolation: 'monotoneX',
+    style: {
+      data: { stroke: '#3182bd', strokeWidth: 1 },
+    },
+  },
+  scatter: {
+    size: 3,
+    style: {
+      data: { fill: '#fff', stroke: '#3182bd', strokeWidth: 1 },
+    },
+  },
+  tooltip: {
+    cornerRadius: 0.8,
+    flyoutStyle: { fill: '#fff' },
+  },
+};
--- src/components/Analytics/AnalyticsVictoryChart/index.js
+++ src/components/Analytics/AnalyticsVictoryChart/index.js
@@ -12,46 +12,8 @@ import {
 } from 'victory';
 import ResponseItem from 'models/ResponseItem';
 import moment from 'moment';
+import AnalyticsVictoryTheme from './AnalyticsVictoryTheme';
 import './AnalyticsVictoryChart.css';
-
-const AnalyticsVictoryTheme = {
-  axis: {
-    style: {
-      axis: { stroke: '#666' },
-      grid: { stroke: '#ccc', strokeDasharray: '5 5' },
-      ticks: { size: 6, stroke: '#666' },
-      tickLabels: { fill: '#666' },
-    },
-  },
-  // bar: {},
-  chart: {
-    domainPadding: 30,
-    padding: { top: 50, right: 50, bottom: 60, left: 60 },
-  },
-  legend: {
-    orientation: 'horizontal',
-    style: {
-      border: { stroke: 'black' },
-      labels: { fontSize: 12 },
-    },
-  },
-  line: {
-    interpolation: 'monotoneX',
-    style: {
-      data: { stroke: '#3182bd', strokeWidth: 1 },
-    },
-  },
-  scatter: {
-    size: 3,
-    style: {
-      data: { fill: '#fff', stroke: '#3182bd', strokeWidth: 1 },
-    },
-  },
-  tooltip: {
-    cornerRadius: 0.8,
-    flyoutStyle: { fill: '#fff' },
-  },
-};
 
 const LegendIcon = props => {
   const { x, y } = props;
