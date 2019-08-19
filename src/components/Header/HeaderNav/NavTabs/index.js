0927dd9d3add6a2a466981d83ef9f57df7cf0656
Jason Crowley <crowley.json@gmail.com>
2019-08-19 12:12:59 -0400

    Fix tab height not matching toolbar height

--- src/components/Header/HeaderNav/NavTabs/index.js
+++ src/components/Header/HeaderNav/NavTabs/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  fullHeight: {
+    ...theme.mixins.toolbar,
+  },
+}));
 
 export default function NavTabs(props) {
   const { tab, handleChangeTab, children } = props;
+  const classes = useStyles();
+
   return (
     <Tabs
       variant="fullWidth"
@@ -14,6 +23,7 @@ export default function NavTabs(props) {
     >
       {children.map((tabProps, index) => (
         <Tab
+          className={classes.fullHeight}
           key={index}
           component={Link}
           {...tabProps}
