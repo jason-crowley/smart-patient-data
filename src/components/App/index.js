740b22e8ed3796797c9da1b92bd527b039ccfdc0
Jason Crowley <crowley.json@gmail.com>
2019-08-20 17:58:22 -0400

    Add basic ready check before rendering AppShell

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Launcher from '../Launcher';
 import AppShell from '../AppShell';
+import { oauth2 as SMART } from 'fhirclient';
 import './App.css';
 
 export default function App() {
+  const [ready, setReady] = useState(false);
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
           <Route exact path="/" component={Launcher} />
-          <Route path="/app" component={AppShell} />
+          <Route
+            path="/app"
+            render={props => {
+              SMART.ready()
+                .then(() => setReady(true))
+                .catch(() => setReady(false));
+              return ready && <AppShell />;
+            }}
+          />
         </Switch>
       </div>
     </BrowserRouter>
