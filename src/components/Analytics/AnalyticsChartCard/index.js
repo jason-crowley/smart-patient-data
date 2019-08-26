1255d06920356ec691d849b9152f3a83fc486adf
Jason Crowley <crowley.json@gmail.com>
2019-08-26 12:31:12 -0400

    Remove CardContent padding in chart cards

--- src/components/Analytics/AnalyticsChartCard/index.js
+++ src/components/Analytics/AnalyticsChartCard/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import AnalyticsVictoryChart from '../AnalyticsVictoryChart';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles(theme => ({
+  cardContent: {
+    padding: theme.spacing(0),
+  },
+}));
+
 export default function AnalyticsChartCard(props) {
+  const classes = useStyles();
   return (
     <Card>
       <CardActionArea>
-        <CardContent>
+        <CardContent className={classes.cardContent}>
           <AnalyticsVictoryChart {...props} />
         </CardContent>
       </CardActionArea>
