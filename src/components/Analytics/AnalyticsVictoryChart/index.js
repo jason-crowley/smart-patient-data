7bb6d52e94cf88928bd451e557e8f5a27ff65a65
Jason Crowley <crowley.json@gmail.com>
2019-08-26 16:16:31 -0400

    Remove 'Date' axis label and chart legend and all unused variables from AnalyticsChart

--- src/components/Analytics/AnalyticsVictoryChart/index.js
+++ src/components/Analytics/AnalyticsVictoryChart/index.js
@@ -4,36 +4,21 @@ import {
   VictoryChart,
   VictoryLabel,
   VictoryAxis,
-  VictoryLegend,
-  Border,
   VictoryGroup,
   VictoryLine,
   VictoryScatter,
-  VictoryBar,
   VictoryTooltip,
 } from 'victory';
-// import { reduce, min, max, map, filter, flatten } from 'ramda';
 import moment from 'moment';
 import AnalyticsVictoryTheme from '../AnalyticsVictoryTheme';
-import AnalyticsLegendIcon from './AnalyticsLegendIcon';
 import { makeSingleDateDomain } from 'utils/domainMakers';
 
-const svgNS = 'http://www.w3.org/2000/svg';
-const svgTag = document.createElementNS(svgNS, 'svg');
-const textTag = document.createElementNS(svgNS, 'text');
-const tspanTag = document.createElementNS(svgNS, 'tspan');
-document.body.appendChild(svgTag);
-svgTag.appendChild(textTag);
-textTag.appendChild(tspanTag);
-tspanTag.setAttribute('font-size', '12');
-tspanTag.setAttribute('font-family', "'Gill Sans', 'Gill Sans MT', 'Ser­avek', 'Trebuchet MS', sans-serif");
-
 export default function AnalyticsVictoryChart(props) {
-  let {
+  const {
     width = 450,
     height = 300,
     animate = false,
-    data: { responseItems, eventData },
+    data: { responseItems },
     onClick,
     ...restProps
   } = props;
@@ -45,44 +30,6 @@ export default function AnalyticsVictoryChart(props) {
   const axisText = (legendText.length < 40)
     ? `${legendText} (${unit})`
     : legendText;
-
-  // Calculate text size for legend
-  const textNode = document.createTextNode(legendText);
-  tspanTag.appendChild(textNode);
-  const textLength = tspanTag.getComputedTextLength();
-  tspanTag.removeChild(textNode);
-
-//   // Filter out events that are not contained within the domain
-//   const times = responseItems.map(({ date }) => date.getTime());
-//   const minTime = reduce(min, Infinity, times);
-//   const maxTime = reduce(max, -Infinity, times);
-//   eventData = map(filter(({ startDate, endDate }) => {
-//     const startTime = startDate.getTime();
-//     const endTime = endDate.getTime();
-//     const startInDomain = minTime < startTime && startTime < maxTime;
-//     const endInDomain = minTime < endTime && endTime < maxTime;
-//     return startInDomain || endInDomain;
-//   }), eventData);
-//   eventData = eventData.filter(events => events.length);
-
-//   // Find min value for event bar height
-//   const values = responseItems.map(({ value }) => value);
-//   const minValue = reduce(min, Infinity, values);
-//   const maxValue = reduce(max, -Infinity, values);
-//   const range = maxValue - minValue;
-//   const step = range * 1.5 / (eventData.length || 1);
-
-//   // Assign a height and color for each grouping of events
-//   eventData = eventData.map((events, i) => {
-//     return events.map(event => (
-//       {
-//         ...event,
-//         height: minValue - step * (i + 1),
-//         color: (i % 2 === 0) ? 'red' : 'green',
-//       }
-//     ));
-//   });
-//   eventData = flatten(eventData);
 
   const timeDomain = (responseItems.length === 1)
     ? makeSingleDateDomain(responseItems[0].date)
@@ -111,38 +58,12 @@ export default function AnalyticsVictoryChart(props) {
           textAnchor="middle"
           style={{ fontSize: 16 }}
         />
-        <VictoryAxis
-          dependentAxis
-          label="Date"
-          axisLabelComponent={<VictoryLabel dy={10} />}
-        />
+        <VictoryAxis dependentAxis />
         <VictoryAxis
           label={axisText}
           axisLabelComponent={<VictoryLabel dy={-15} />}
           tickFormat={d => `${d.toFixed(2)}`}
         />
-        {/* <VictoryBar */}
-        {/*   data={eventData} */}
-        {/*   x="height" */}
-        {/*   y0="startDate" */}
-        {/*   y="endDate" */}
-        {/*   barWidth={8} */}
-        {/*   labels={({ startDate, endDate, code: { text } }) => { */}
-        {/*     const start = moment(startDate).format('MM/DD/YY'); */}
-        {/*     const end = moment(endDate).format('MM/DD/YY'); */}
-        {/*     const dates = `${start}${(start === end) ? '' : ' - ' + end}`; */}
-        {/*     return `${text}: ${dates}`; */}
-        {/*   }} */}
-        {/*   labelComponent={ */}
-        {/*     <VictoryTooltip */}
-        {/*       cornerRadius={2} */}
-        {/*       dy={4} */}
-        {/*       flyoutStyle={{ fill: '#fff' }} */}
-        {/*       orientation="top" */}
-        {/*       pointerLength={5} */}
-        {/*     /> */}
-        {/*   } */}
-        {/* /> */}
         <VictoryGroup
           data={responseItems}
           x="value"
@@ -165,14 +86,6 @@ export default function AnalyticsVictoryChart(props) {
           <VictoryLine sortKey="date" animate={animate} />
           <VictoryScatter />
         </VictoryGroup>
-        <VictoryLegend
-          x={90} y={50}
-          borderComponent={<Border width={textLength + 35} />}
-          data={[{ name: legendText }]}
-          dataComponent={<AnalyticsLegendIcon />}
-          labelComponent={<VictoryLabel dy={1} />}
-          style={{ border: { fill: 'white' } }}
-        />
       </VictoryChart>
     </div>
   );
