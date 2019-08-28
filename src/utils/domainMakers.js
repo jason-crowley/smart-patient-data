469df45cff22ffea98d9ced9ec4ec82ca2ed06b9
Jason Crowley <crowley.json@gmail.com>
2019-08-28 11:37:40 -0400

    Refactor domainMakers and add convenience function for all cases

--- src/utils/domainMakers.js
+++ src/utils/domainMakers.js
@@ -7,12 +7,27 @@ const makeDefaultDomain = () => ([
 ]);
 
 // Make domain around given single date
-const makeSingleDateDomain = datePoint => ([
-  moment(datePoint).subtract(0.5, 'days').toDate(),
-  moment(datePoint).add(0.5, 'days').toDate(),
+const makeSingleDateDomain = date => ([
+  moment(date).subtract(0.5, 'days').toDate(),
+  moment(date).add(0.5, 'days').toDate(),
 ]);
+
+const makeMultipleDatesDomain = dates => {
+  const times = dates.map(date => +date);
+  const minTime = Math.min(...times);
+  const maxTime = Math.max(...times);
+  return [new Date(minTime), new Date(maxTime)];
+};
+
+const makeDomainFromDates = dates => {
+  return (dates.length === 1)
+    ? makeSingleDateDomain(dates[0])
+    : (dates.length ? makeMultipleDatesDomain(dates) : makeDefaultDomain())
+};
 
 export {
   makeDefaultDomain,
   makeSingleDateDomain,
+  makeMultipleDatesDomain,
+  makeDomainFromDates,
 };
